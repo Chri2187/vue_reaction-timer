f15c9c5c3155c3ef50a49dd65298f077ce9ade0e
Christian Francini Mascagni <christian.francini87@gmail.com>
2023-11-17 00:04:16 +0100

    Starter Template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Ninja Reaction Timer</h1>
+    <button @click="start" :disabled="isActive">Play</button>
+    <Block v-if="isActive" :delay="delay" @end="endGame" />
+    <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: { Block, Results },
+    data() {
+        return {
+            isActive: false,
+            delay: null,
+            score: null,
+            showResults: false,
+        };
+    },
+    methods: {
+        start() {
+            this.delay = 2000 + Math.random() * 5000;
+            this.isActive = true;
+            this.showResults = false;
+        },
+        endGame(rTime) {
+            this.score = rTime;
+            this.isActive = false;
+            this.showResults = true;
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: black;
+    margin-top: 60px;
+}
+button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    padding: 8px 16px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+}
+button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="result">Reaction Time: {{ score }} ms</div>
+    <p class="rank">{{ rank }}</p>
+</template>
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        };
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Ninja Fingers';
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes';
+        } else {
+            this.rank = 'Snail Pace...';
+        }
+    },
+};
+</script>
+<style>
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+.result {
+    width: 200px;
+    border-radius: 20px;
+    background: #dfe0e0;
+    box-shadow: 5px 5px 5px grey;
+    color: black;
+    text-align: center;
+    padding: 20px 0;
+    margin: 40px auto;
+}
+</style>
